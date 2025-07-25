8f9d4002c078baf3d51b9f97a5e7cc233f5d820d
rafi570md <rafipersonal535@gmail.com>
2025-07-25 14:36:25 +0600

    oddeven

---
+++ OddEven.java
@@ -0,0 +1,29 @@
+public class OddEven{
+    public static void main(String[] args) {
+    int a=9; 
+    int b=10;
+    
+   
+    
+    if(a%2== 0)
+    {
+        System.out.println("a is an Even number!");
+    }
+    else
+    {
+        System.out.println("a is a Odd number!");
+    }
+    
+  
+    
+    if(b%2== 0)
+    {
+        System.out.println("b is an Even number!");
+    }
+    else
+    {
+        System.out.println("b is a Odd number!");
+    }
+
+    }
+}
